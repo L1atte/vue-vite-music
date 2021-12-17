86455349ec985d97048314eda50b187fbc3b70a4
L1atte <1454841541@qq.com>
2021-12-17 22:04:32 +0800

    no message

--- backend/music/static/js/recommend-68ad9582.js
+++
@@ -1,1 +0,0 @@
-import{g as e}from"./index-c79beb1a.js";function i(){return e("/api/getRecommend")}function m(t){return e("/api/getAlbum",{id:t.id})}export{m as a,i as g};
--- backend/music/static/js/singer-e0deba94.js
+++
@@ -1,1 +0,0 @@
-import{g as e}from"./index-c79beb1a.js";function g(){return e("/api/getSingerList")}function n(i){return e("/api/getSingerDetail",{mid:i.mid})}export{n as a,g};
