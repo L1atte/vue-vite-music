ec96a475fd0d761059ce16715da47e15a7701c9a
L1atte <1454841541@qq.com>
2021-12-18 03:10:13 +0800

    no message

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,14 @@
  * @Author: Latte
  * @Date: 2021-12-13 23:05:03
  * @LAstEditors: Latte
- * @LastEditTime: 2021-12-18 02:05:43
+ * @LastEditTime: 2021-12-18 02:59:26
  * @FilePath: \vue-vite-music\backend\index.js
  */
 const express = require("express");
 const compression = require("compression");
 const cookieParser = require("cookie-parser");
 
-const port = process.env.PORT || 9002;
+const port = 3000;
 
 const registerRouter = require("./router");
 
